d17a2860863cdd027464434e79d753460585e64f
Marcus Kida <marcus@kida.io>
2018-07-29 17:08:16 +0200

    Initial Commit

---
+++ RxLocationPermission/RxLocationPermission.h
@@ -0,0 +1,19 @@
+//
+//  RxLocationPermission.h
+//  RxLocationPermission
+//
+//  Created by Marcus Kida on 29.07.18.
+//  Copyright © 2018 Marcus Kida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxLocationPermission.
+FOUNDATION_EXPORT double RxLocationPermissionVersionNumber;
+
+//! Project version string for RxLocationPermission.
+FOUNDATION_EXPORT const unsigned char RxLocationPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxLocationPermission/PublicHeader.h>
+
+
